9515055dcae48350cae149b9159023078d6800aa
Asliddin <asliddinrashidovich7@gmail.com>
2026-02-01 08:38:31 +0500

    some changes

--- strapi/my-project/config/plugins.ts
+++ strapi/my-project/config/plugins.ts
@@ -1,12 +1,1 @@
-export default ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'supabase',
-      providerOptions: {
-        url: env('SUPABASE_URL'),
-        apiKey: env('SUPABASE_API_KEY'),
-        bucket: env('SUPABASE_BUCKET'),
-      },
-    },
-  },
-});
+export default ({}) => ({});
